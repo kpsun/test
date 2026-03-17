46b85a1eefb913a6f3bd964c854ddc233933a099
kpsun <kps2confirm@gmail.com>
2026-03-17 12:08:10 +1100

    Initial website commit

---
+++ js/components.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+const elements = document.querySelectorAll("[data-include]");
+
+elements.forEach(function(el) {
+
+const file = el.getAttribute("data-include");
+
+fetch(file)
+.then(response => response.text())
+.then(data => {
+el.innerHTML = data;
+});
+
+});
+
+});
