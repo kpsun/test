e14fa28dfb6fe657ffd4e9e7bc5e3a3ba99e3e69
kpsun <kps2confirm@gmail.com>
2026-03-17 13:32:10 +1100

    all

--- js/components.js
+++ js/components.js
@@ -1,17 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-const elements = document.querySelectorAll("[data-include]");
-
-elements.forEach(function(el) {
-
-const file = el.getAttribute("data-include");
-
-fetch(file)
-.then(response => response.text())
-.then(data => {
-el.innerHTML = data;
-});
-
-});
-
+	const elements = document.querySelectorAll("[data-include]");
+	elements.forEach(function(el) {
+		const file = el.getAttribute("data-include");
+		fetch(file)
+		.then(response => response.text())
+		.then(data => {
+			el.innerHTML = data;
+		});
+	});
 });
